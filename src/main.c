70be939e9a89206d169de773dc894a956a738d2e
Nathãn Vitor de Lima <nathan2001lima@gmail.com>
2021-09-12 19:22:37 -0300

    Simpe command line help argument

--- src/main.c
+++ src/main.c
@@ -1,11 +1,31 @@
-#include "util.h"
 #include "minesweeper_game.h"
 #include <terminal_frontend.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <time.h>
 
-int main()
+int main(int argc, const char *argv[])
 {
+    if (argc > 1)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            const char *arg = argv[i];
+
+            if (strcmp(arg, "--help") == 0)
+            {
+                printf("Minesweeper 0.1.0\n");
+                return 0;
+            }
+            else
+            {
+                printf("Usage: minesweeper [--help]\n");
+                return 1;
+            }
+        }
+    }
+
     srand(time(NULL));
 
     struct MinesweeperGame game = newMinesweeperGame((struct Pos){8, 8}, 16);
